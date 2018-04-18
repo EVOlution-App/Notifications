6ccc596921255474e7b870c429020eba6ba88e51
Thiago Holanda <unnamedd@gmail.com>
2018-04-19 01:36:49 +0200

    added OneSignal model

--- Sources/App/Models/OneSignal.swift
+++ Sources/App/Models/OneSignal.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 struct OneSignal {
-    enum OS: Int, RawRepresentable {
+    public enum OS: Int, RawRepresentable {
         case ios = 0
         case macos = 9
         case safari = 7
@@ -29,6 +29,16 @@ struct OneSignal {
             default: return nil
             }
         }
+        
+        public var value: String {
+            switch self {
+            case .ios: return "ios"
+            case .macos: return "macos"
+            case .safari: return "safari"
+            case .chrome: return "chrome"
+            case .firefox: return "firefox"
+            }
+        }
     }
 
     enum TestType: Int {
@@ -39,12 +49,12 @@ struct OneSignal {
     struct Device {
         let appID: String
         let identifier: String
-        let language: String
-        let platform: OS
-        let deviceOS: String
-        let deviceModel: String
-        let appVersion: String
-        let testType: TestType
+        let language: String?
+        let platform: OS?
+        let deviceOS: String?
+        let deviceModel: String?
+        let appVersion: String?
+        let testType: TestType?
 
         struct Keys {
             static var appID        : String = "app_id"
@@ -57,6 +67,22 @@ struct OneSignal {
             static let testType     : String = "test_type"
         }
     }
+    
+    struct Response {
+        let id: String
+        let success: Bool
+        
+        init?(_ json: JSON) {
+            guard
+                let id = json["id"]?.string,
+                let success = json["success"]?.bool else {
+                return nil
+            }
+            
+            self.id = id
+            self.success = success
+        }
+    }
 }
 
 extension OneSignal.Device: NodeRepresentable {
@@ -66,13 +92,75 @@ extension OneSignal.Device: NodeRepresentable {
         
         try node.set(keys.appID, appID)
         try node.set(keys.identifier, identifier)
-        try node.set(keys.language, language)
-        try node.set(keys.platform, platform)
-        try node.set(keys.deviceOS, deviceOS)
-        try node.set(keys.deviceModel, deviceModel)
-        try node.set(keys.appVersion, appVersion)
-        try node.set(keys.testType, testType)
+        
+        if let value = language {
+            try node.set(keys.language, value)
+        }
+        
+        if let value = platform {
+            try node.set(keys.platform, value.rawValue)
+        }
+        
+        if let value = deviceOS {
+            try node.set(keys.deviceOS, value)
+        }
+        
+        if let value = deviceModel {
+            try node.set(keys.deviceModel, value)
+        }
+        
+        if let value = appVersion {
+            try node.set(keys.appVersion, value)
+        }
+        
+        if let value = testType {
+            try node.set(keys.testType, value.rawValue)
+        }
         
         return node
+    }
+    
+    func makeJSON() throws -> JSON {
+        let node = try makeNode(in: nil)
+        let json = JSON(node: node)
+        
+        return json
+    }
+}
+
+extension OneSignal.Device {
+    static func create(with token: String, using json: JSON) -> OneSignal.Device? {
+        guard let appID = Environment.Keys.OneSignal.app else {
+            return nil
+        }
+        
+        guard
+            let code = json["platform"]?.string,
+            let platform = OneSignal.OS(code) else {
+                return nil
+        }
+        
+        let language     = json["language"]?.string
+        let model        = json["model"]?.string
+        let os           = json["os"]?.string
+        let appVersion   = json["appVersion"]?.string
+        
+        var testType: OneSignal.TestType?
+        if let value = json["test"]?.bool, value {
+            testType = .development
+        }
+
+        let device = OneSignal.Device(
+            appID: appID,
+            identifier: token,
+            language: language,
+            platform: platform,
+            deviceOS: os,
+            deviceModel: model,
+            appVersion: appVersion,
+            testType: testType
+        )
+        
+        return device
     }
 }
